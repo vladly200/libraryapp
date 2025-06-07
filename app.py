cbacd575685a43348794cbea70be16ff94892eaf
Volodymyr <dmimportmail@gmail.com>
2025-06-07 21:23:30 +0200

    test button

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/handle_click',methods = ['POST'])
+def handle_click():
+    print("Button was clicked!")
+    return "Button was clicked!"
 
 if __name__ == "__main__":
     app.run()
