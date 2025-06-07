b0f6e03835038f8616082e6dd4449a0dfe9547e0
Volodymyr <dmimportmail@gmail.com>
2025-06-07 21:37:14 +0200

    bug fix

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 @app.route('/handle_click',methods = ['POST'])
 def handle_click():
     print("Button was clicked!")
     return "Button was clicked!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
