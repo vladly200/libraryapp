441b5bfa878b7beeab463c0ff2534ceac22e2630
Volodymyr <dmimportmail@gmail.com>
2025-06-07 21:48:51 +0200

    folder update

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-@app.route('/handle_click',methods = ['POST'])
-def handle_click():
-    print("Button was clicked!")
-    return "Button was clicked!"
+    return 'Hello Wrold'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
