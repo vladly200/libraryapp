70a3814a144df78761c2afe74a197b67ab5b3ed3
vladly200 <vladly200@gmail.com>
2026-04-22 00:53:50 +0200

    Fixxxxxxx

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
-@app.route('/booksrch', methods=['GET'])
-def search_books():
-    query = request.args.get('search', '').strip()
-    books = []
-    if query:
-        try:
-            conn = get_db_connection()
-            with conn.cursor() as cursor:
-                sql = "SELECT name, year_of_release, author FROM book WHERE name LIKE %s"
-                cursor.execute(sql, ('%' + query + '%',))
-                books = cursor.fetchall()
-            conn.close()
-        except Exception as e:
-            # Если база не пустит, ты увидишь причину прямо на сайте!
-            return f"""
-            <div style="color: red; border: 1px solid red; padding: 20px;">
-                <h1>Ошибка подключения к базе:</h1>
-                <p>{str(e)}</p>
-                <p>Проверь DB_USER и DB_PASSWORD в настройках Azure!</p>
-            </div>
-            """
-    return render_template('booksrch.html', books=books, query=query)
+import os
+import pymysql  # <--- ВОТ ЭТОЙ СТРОЧКИ У ТЕБЯ СЕЙЧАС НЕТ
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+def get_db_connection():
+    return pymysql.connect(
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME'),
+        port=3306,
+        cursorclass=pymysql.cursors.DictCursor,
+        connect_timeout=5, # Не ждем дольше 5 секунд
+        ssl={'ssl': {}}    # Обязательно для Azure Flexible Server
+    )
